cffd122e232d6c566a7b223f62bf446fbc7744ec
andres <valentinomonzonandres@gmail.com>
2023-09-18 13:36:13 -0300

    feat: add section slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import { WelcomeSection } from './components'
+import './home.css'
 
 export default function Home () {
   return (
-    <main>
-      <WelcomeSection />
+    <main className='ralative w-full h-screen overflow-auto snap-y snap-mandatory'>
+      <div className='imgUrl'>
+        <div className='snap-center'>
+          <WelcomeSection />
+        </div>
+        <div className='snap-center'>
+          <WelcomeSection />
+        </div>
+        <div className='snap-center'>
+          <WelcomeSection />
+        </div>
+      </div>
     </main>
   )
 }
