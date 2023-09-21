1fe50d9ec392963d9765432d4450f025c30d308a
andres <valentinomonzonandres@gmail.com>
2023-09-20 22:00:41 -0300

    fix: Fix mobile navigation menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -28,10 +28,18 @@ function Navbar () {
   const [click, setClick] = useState(false)
   const { activeLinkId } = useNavContext()
 
+  const openMenu = () => {
+    setClick(true)
+  }
+
+  const closeMenu = () => {
+    setClick(false)
+  }
+
   return (
     <header className='w-full py-5 fixed flex items-center z-[100]'>
       <div className='sm:hidden w-full flex items-center justify-end px-3'>
-        <button className='backdrop-blur bg-zinc-800/20 p-2' onClick={() => { setClick(true) }}>
+        <button aria-label='Menu' className='backdrop-blur bg-zinc-800/20 p-2' onClick={openMenu}>
           <IconMenuDeep />
         </button>
       </div>
@@ -43,7 +51,7 @@ function Navbar () {
             Routes.map((route) => {
               return (
                 <li key={route.href}>
-                  <Link onClick={() => { setClick(false) }} className={`relative hover-lines py-1 px-4 font-normal text-gray-200 text-sm rounded ${activeLinkId === route.href ? 'active' : ''}`} href={`#${route.href}`}>{route.name}</Link>
+                  <Link scroll={false} className={`relative hover-lines py-1 px-4 font-normal text-sm rounded ${activeLinkId === route.href ? 'active' : ''}`} href={`#${route.href}`}>{route.name}</Link>
                 </li>
               )
             })
@@ -52,10 +60,10 @@ function Navbar () {
       </nav>
 
       {/* phone */}
-      <nav className={`h-screen sm:hidden fixed p-5 w-[90%] top-0 right-0 translate-x-[100%] duration-300 bg-black/50 backdrop-blur-lg flex flex-col gap-10 items-end ${click ? 'translate-x-[0%]' : ''}`}>
+      <nav className={`min-h-screen sm:hidden fixed p-5 w-[90%] top-0 right-0 duration-300 bg-black/50 backdrop-blur-lg flex flex-col gap-10 items-end ${click ? 'translate-x-[0%]' : 'translate-x-[100%]'}`}>
         <header className='flex items-center justify-center w-full relative'>
           <h2 className='text-xl text-center'>Menu</h2>
-          <button className='absolute right-2' onClick={() => { setClick(false) }}>
+          <button aria-label='Close Menu' className='absolute right-2' onClick={closeMenu}>
             <IconX />
           </button>
         </header>
@@ -64,7 +72,7 @@ function Navbar () {
             Routes.map((route) => {
               return (
                 <li key={route.href}>
-                  <Link onClick={() => { setClick(false) }} className={`relative hover-lines py-1 px-4 font-normal text-gray-200 text-sm rounded ${activeLinkId === route.href ? 'active' : ''}`} href={`#${route.href}`}>{route.name}</Link>
+                  <Link scroll={false} onClick={() => { setClick(false) }} className={`relative hover-lines py-1 px-4 font-normal text-gray-200 text-sm rounded ${activeLinkId === route.href ? 'active' : ''}`} href={`#${route.href}`}>{route.name}</Link>
                 </li>
               )
             })
